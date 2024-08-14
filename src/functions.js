7d93c9c28637fc9539f56e984d260f42bf3f0c3f
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2024-08-14 20:29:34 -0300

    feat: part one of work
    Co-authored-by: JoaoVictorPires <joaovictorsantos0213@gmail.com>
    Co-authored-by: JeremiasONunes <jeremias.6292@gmail.com>
    Co-authored-by: Gustavohaa <gustavohaa7@gmail.com>
    Co-authored-by: Rafael-Mesquita <rafael_senac@hotmail.com>

---
+++ src/functions.js
@@ -0,0 +1,70 @@
+const fs = require('fs').promises;
+
+const SCORES_FILE = './src/scores.json';
+
+const writeScoresFile = async (data) => {
+  await fs.writeFile(SCORES_FILE, JSON.stringify(data, null, '\t'));
+};
+
+const createScoresFile = async () => {
+  fs.open(SCORES_FILE, 'r')
+    .then(async (fd) => {
+      await fd.close();
+    })
+    .catch(async (error) => {
+      if (error.code === 'ENOENT') {
+        await writeScoresFile([]);
+
+        console.log('Scores file created!');
+      }
+    });
+};
+
+const readScoresFile = async () => {
+  const data = await fs.readFile(SCORES_FILE, {
+    encoding: 'utf8',
+  });
+
+  return JSON.parse(data);
+};
+
+const addScore = async (team, score) => {
+  if (!team || typeof team !== 'string') {
+    throw new Error('Team are required or must be a string!');
+  }
+
+  if (typeof score !== 'number') {
+    throw new Error('Score are required or must be a number!');
+  }
+
+  if (score > 3 || score < 1) {
+    throw new Error('Score must be between 1 and 3!');
+  }
+
+  const scores = await readScoresFile();
+
+  const teamIndex = scores.findIndex((item) => item.team === team);
+
+  if (teamIndex !== -1) {
+    scores[teamIndex].score += score;
+  } else {
+    scores.push({
+      team,
+      score,
+    });
+  }
+
+  await writeScoresFile(scores);
+};
+
+const getScoresAscending = async () => {
+  const scores = await readScoresFile();
+
+  return scores.sort((a, b) => a.score - b.score);
+};
+
+module.exports = {
+  createScoresFile,
+  addScore,
+  getScoresAscending,
+};
